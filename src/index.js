c6d8f839bd4a79c74ce541c87ba27134463e7ca6
dominikpollak <dominikpollak17@gmail.com>
2024-05-15 19:54:53 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -6,35 +6,48 @@ const btn = document.getElementsByClassName("jamonbread");
 
 for (let i = 0; i < btn.length; i++) {
   const config = JSON.parse(btn[i].dataset.config);
-  (async () => {
-    const fingerprintRes = await translateFingerprint(config.fingerprint);
 
-    const res = await fetchNftListing(
-      fingerprintRes.policyId,
-      fingerprintRes.assetName
-    );
+  if (config.policyid && jamConfig.buttonType === "collectionOffer") {
+    console.log(jamConfig.buttonLabel);
+    btn[i].innerHTML = jamConfig.buttonLabel;
 
     Object.entries(buttonStyles).forEach(([key, value]) => {
       btn[i].style[key] = value;
     });
+  } else {
+    (async () => {
+      const fingerprintRes = await translateFingerprint(config.fingerprint);
 
-    if (!res.sellOrder && jamConfig.buttonType === "buy") {
-      if (jamConfig.fallbackButtonLabel) {
-        btn[i].innerHTML = jamConfig.fallbackButtonLabel;
-        Object.entries(fallbackButtonStyles).forEach(([key, value]) => {
-          btn[i].style[key] = value;
-        });
+      const res = await fetchNftListing(
+        fingerprintRes.policyId,
+        fingerprintRes.assetName
+      );
+
+      Object.entries(buttonStyles).forEach(([key, value]) => {
+        btn[i].style[key] = value;
+      });
+
+      if (!res.sellOrder && jamConfig.buttonType === "buy") {
+        if (jamConfig.fallbackButtonLabel) {
+          btn[i].innerHTML = jamConfig.fallbackButtonLabel;
+          Object.entries(fallbackButtonStyles).forEach(([key, value]) => {
+            btn[i].style[key] = value;
+          });
+        }
+      } else {
+        btn[i].innerHTML = jamConfig.buttonLabel;
       }
-    } else {
-      btn[i].innerHTML = jamConfig.buttonLabel;
-    }
-
-    console.log("res", res);
-  })();
+    })();
+  }
 
   btn[i].addEventListener("click", async (e) => {
-    // const iframeSrc = `https://mainnet-stage.jamonbread.tech/iframe/asset/${config.fingerprint}?theme=dark`;
-    const iframeSrc = `https://mainnet-stage.jamonbread.tech/iframe/asset/${config.fingerprint}?theme=${jamConfig.theme}&type=${jamConfig.buttonType}&showPopup=${jamConfig.showPopup}`;
+    let iframeSrc = "";
+    if (jamConfig.buttonType === "collectionOffer") {
+      iframeSrc = `https://mainnet-stage.jamonbread.tech/iframe/collectionOffer/${config.policyid}?theme=${jamConfig.theme}&showPopup=${jamConfig.showPopup}`;
+    } else {
+      iframeSrc = `https://mainnet-stage.jamonbread.tech/iframe/asset/${config.fingerprint}?theme=${jamConfig.theme}&type=${jamConfig.buttonType}&showPopup=${jamConfig.showPopup}`;
+    }
+
     const newWindow = window.open(iframeSrc, "_blank");
     setTimeout(function () {
       newWindow.postMessage(
