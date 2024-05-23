64bbbcd1d09704d43a11bc7baaeb5282f901ed1e
dominikpollak <dominikpollak17@gmail.com>
2024-05-23 16:45:30 +0200

    wip

--- dist/bundle.js
+++ dist/bundle.js
@@ -704,9 +704,20 @@ const translateFingerprint = async (fingerprint) => {
   return data;
 };
 
-// https://jamonbread.io/asset
+const url = jamConfig.testnet
+  ? "https://testnet-stage.jamonbread.tech/iframe"
+  : "https://mainnet-stage.jamonbread.tech/iframe";
 
 const btn = document.getElementsByClassName("jamonbread");
+const iframeDiv = document.getElementById("iframe_list_div");
+
+if (iframeDiv) {
+  const iframeConfig = JSON.parse(iframeDiv.dataset.config);
+  const listIframe = document.createElement("iframe");
+  listIframe.src = `${url}/collectionList/${iframeConfig.policyId}?theme=${jamConfig.theme}&lu=${jamConfig.logoUrl}&ls=${jamConfig.logoSize}&pn=${jamConfig.projectName}&nfs=${jamConfig.nameFontSize}&dv=${jamConfig.defaultView}&a=${jamConfig.affilCode}`;
+  listIframe.className = "job_list_iframe";
+  iframeDiv.appendChild(listIframe);
+}
 
 for (let i = 0; i < btn.length; i++) {
   const config = JSON.parse(btn[i].dataset.config);
@@ -714,7 +725,12 @@ for (let i = 0; i < btn.length; i++) {
   if (config.policyid && config.buttonType === "collectionOffer") {
     btn[i].innerHTML = config.buttonLabel;
     btn[i].style.display = "block";
-  } else {
+  } else if (
+    config.fingerprint &&
+    (config.buttonType === "buy" ||
+      config.buttonType === "offer" ||
+      config.buttonType === "list")
+  ) {
     (async () => {
       const fingerprintRes = await translateFingerprint(config.fingerprint);
 
@@ -723,7 +739,25 @@ for (let i = 0; i < btn.length; i++) {
         fingerprintRes.assetName
       );
 
-      if (!res.sellOrder && jamConfig.buttonType === "buy") {
+      // If user doesn't own the asset, don't show the list button
+      if (
+        res.owner.address &&
+        res.owner.address !== jamConfig.wallet &&
+        config.buttonType === "list"
+      ) {
+        return;
+      }
+
+      // If user is listed by the user, don't show the buy button
+      if (
+        res.sellOrder &&
+        res.sellOrder.listedByAddress === jamConfig.wallet &&
+        config.buttonType === "buy"
+      ) {
+        return;
+      }
+
+      if (!res.sellOrder && config.buttonType === "buy") {
         if (config.fallbackButtonLabel) {
           btn[i].innerHTML = config.fallbackButtonLabel;
           btn[i].classList.remove("job_asset_button");
@@ -741,11 +775,10 @@ for (let i = 0; i < btn.length; i++) {
 
   btn[i].addEventListener("click", async (e) => {
     let iframeSrc = "";
-    // if (jamConfig.buttonType === "collectionOffer") {
     if (btn[i].classList.contains("job_collectionInfo_button")) {
-      iframeSrc = `http://localhost:3000/iframe/collectionOffer/${config.policyid}?theme=${jamConfig.theme}&showPopup=${jamConfig.showPopup}`;
-    } else {
-      iframeSrc = `http://localhost:3000/iframe/asset/${config.fingerprint}?theme=${jamConfig.theme}&type=${jamConfig.buttonType}&showPopup=${jamConfig.showPopup}`;
+      iframeSrc = `${url}/collectionOffer/${config.policyid}?theme=${jamConfig.theme}&showPopup=${jamConfig.showPopup}`;
+    } else if (btn[i].classList.contains("job_asset_button")) {
+      iframeSrc = `${url}/asset/${config.fingerprint}?theme=${jamConfig.theme}&type=${jamConfig.buttonType}&showPopup=${jamConfig.showPopup}`;
     }
 
     const newWindow = window.open(iframeSrc, "_blank");
